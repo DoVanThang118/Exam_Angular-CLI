1e3a62855fb232b392f3fc3cfe396e8e67f120a0
DoVanThang118 <102956076+DoVanThang118@users.noreply.github.com>
2022-06-23 08:45:01 +0700

    exam

--- src/app/Exam/exam.component.ts
+++ src/app/Exam/exam.component.ts
@@ -8,87 +8,28 @@ import {ExamService} from "../services/exam.service";
   styleUrls: ['./exam.component.css']
 })
 export class ExamComponent {
-  categories: ICategory[] = [
-    {
-      "name": "Margaret",
-      "avatar": "1.jpg"
-    },
-    {
-      "name": "Laura",
-      "avatar": "2.jpg"
-    },
-    {
-      "name": "Robert",
-      "avatar": "3.jpg"
-    },
-    {
-      "name": "Albert",
-      "avatar": "4.jpg"
-    },
-    {
-      "name": "Michale",
-      "avatar": "5.jpg"
-    }
-  ];
-  mails: IMail[] = [
-    {
-      "name": "Margaret",
-      "avatar": "1.jpg",
-      "title": "dsd sddcdef dsfdc",
-      "hiredate": "13/4/1995",
-      "address": "3 efdf sdcsdc",
-      "city": "pari",
-      "phone": "(34)43434-343324"
-    },
-    {
-      "name": "Laura",
-      "avatar": "2.jpg",
-      "title": "dsd sddcdef dsfdc",
-      "hiredate": "13/4/1995",
-      "address": "3 efdf sdcsdc",
-      "city": "pari",
-      "phone": "(34)43434-343324"
-    },
-    {
-      "name": "Robert",
-      "avatar": "3.jpg",
-      "title": "dsd sddcdef dsfdc",
-      "hiredate": "13/4/1995",
-      "address": "3 efdf sdcsdc",
-      "city": "pari",
-      "phone": "(34)43434-343324"
-    },
-    {
-      "name": "Albert",
-      "avatar": "4.jpg",
-      "title": "dsd sddcdef dsfdc",
-      "hiredate": "13/4/1995",
-      "address": "3 efdf sdcsdc",
-      "city": "pari",
-      "phone": "(34)43434-343324"
-    },
-    {
-      "name": "Michale",
-      "avatar": "5.jpg",
-      "title": "dsd sddcdef dsfdc",
-      "hiredate": "13/4/1995",
-      "address": "3 efdf sdcsdc",
-      "city": "pari",
-      "phone": "(34)43434-343324"
-    }
-  ];
+  categories: ICategory[] = [];
+  mails: IMail[] = [];
 
-  // constructor(private  examService: ExamService) {
-  // }
-  //
-  // ngOnInit(){
-  //   this.examService.maillist().subscribe(value => {
-  //     this.categories = value.data.categories;
-  //     this.mails = value.data.mails
-  //   })
-  // }
-  //
-  // chageMails(category: string,){
-  //
-  // }
+   constructor(private  examService: ExamService) {
+   }
+
+   ngOnInit(){
+     this.examService.maillist().subscribe(value => {
+       this.categories = value.data.categories;
+       this.mails = value.data.mails
+     })
+   }
+
+   chageMails(name: string, avatar: string){
+     var new_list: IMail[] = [];
+     this.examService.maillist().subscribe(value => {
+       for (var i=0;i<value.data.mails.length;i++){
+         if (value.data.mails[i].name == name && value.data.mails[i].avatar == avatar){
+           new_list.push(value.data.mails[i]);
+         }
+       }
+       this.mails = new_list;
+     })
+   }
 }
